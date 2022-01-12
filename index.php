59b46916a6f9ae7252b420246dc7ba55307883a1
= <thomas.graham.97@gmail.com>
2022-01-11 20:58:04 -0500

    More static files
    Including a basic version of ecoto.json

--- index.php
+++ index.php
@@ -11,17 +11,24 @@
 		<script src = "eco.js"></script>
 	</head>
 	<body>
-		<h1>Eco T.O</h1>
-		<h2>Minimum viable solution</h2>
-		
-		<div id = "question-interface">
+		<nav>
+			<h1>Eco T.O</h1>
+			<h2>Minimum viable solution</h2>
+		</nav>
+		<section id = "question-interface">
 			<h3>Question</h3>
 			<p id = "question">Question</p>
 			<form id = "question-form">
+				<label for  = "boolean-0">Yes</label>
+				<input type = "radio" id = "boolean-0" name = "boolean">
+				<label for  = "boolean-0">No</label>
+				<input type = "radio" id = "boolean-1" name = "boolean">
+				<input type = "submit" id = "question-form-skip" value = "Skip">
+				<input type = "submit" id = "question-form-submit" value = "Submit">
 			</form>
-		</div>
-		<div id = "matches-interface">
+		</section>
+		<section id = "matches-interface">
 			<h3 id = "matches-counter">Matches</h3>
-		</div>
+		</section>
 	</body>
 </html>
