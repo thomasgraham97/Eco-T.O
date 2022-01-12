4799eefe7963e558b3fab118a9dccf36c72c52d4
= <thomas.graham.97@gmail.com>
2022-01-11 19:14:43 -0500

    First static

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Eco T.O (MVS)</title>
+		<!-- Meta tags -->
+		<meta charset = "utf-8">
+		<meta name = "viewport" content = "width=device-width, initial-scale=1">
+		
+		<!-- Scripts -->
+		<script src = "eco.js"></script>
+	</head>
+	<body>
+		<h1>Eco T.O</h1>
+		<h2>Minimum viable solution</h2>
+		
+		<div id = "question-interface">
+			<h3>Question</h3>
+			<p id = "question">Question</p>
+			<form id = "question-form">
+			</form>
+		</div>
+		<div id = "matches-interface">
+			<h3 id = "matches-counter">Matches</h3>
+		</div>
+	</body>
+</html>
